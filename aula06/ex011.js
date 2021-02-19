9aa450db684e6a61c3387daec074f844b80c9280
Matheus Nunes <mathnunes95@gmail.com>
2021-02-19 19:10:18 -0300

    ex11,12,13

--- aula06/ex011.js
+++ aula06/ex011.js
@@ -2,9 +2,9 @@ var idade = 22
 if(idade < 18){
     console.log("Não vota")
 }
-else if(idade >= 16 && idade < 18) {
+else if(idade >= 16 && idade < 18 || idade > 65) {
     console.log("Voto opcional")
 }
 else{
-    console.log("Vota")
+    console.log("Voto obrigatório")
 }
